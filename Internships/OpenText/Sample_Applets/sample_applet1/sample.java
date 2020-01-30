dde254c4e4f276d2b6816ab0ca26f51bd8298390
Prudhvi Raj <41112634+BhashampallyPrudhviRaj@users.noreply.github.com>
2020-01-30 16:20:20 +0530

    Delete sample.java

--- Internships/OpenText/Sample_Applets/sample_applet1/sample.java
+++
@@ -1,8 +0,0 @@
-package sample_applet1;
-import java.awt.*;
-import java.applet.*;
-public class sample extends Applet{
-	public void paint(Graphics g) {
-	g.drawString("Hi Prudhvi", 50, 50);
-	}
-}
